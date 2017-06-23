231e413fc7bfef69aaa4d5eecba73a71513f3a40
qzhang0 <QiZhang864@Gmail.com>
2017-06-22 22:35:44 -0400

    java and javascript solutions for subsets

---
+++ src/subsets/solution1.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        subsetsHelper(results, new ArrayList<>(), nums, 0);
+        
+        return results;
+    }
+    
+    void subsetsHelper(List<List<Integer>> results, List<Integer> list, int[] nums, int startIndex) {
+        results.add(new ArrayList<>(list));
+        
+        for (int i = startIndex; i < nums.length; ++i) {
+            list.add(nums[i]);
+            subsetsHelper(results, list, nums, i + 1); // does not really understand why use i + 1
+            list.remove(list.size() - 1);
+        }
+    }
+}
